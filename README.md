006e8e84ceaa3ba70192cf9426ab80e003a0836c
Dawn <207631160+KeeningDawn@users.noreply.github.com>
2026-08-19 23:24:12 -0500

    chore: upload template

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hunger in Peace
+
+## Setup
+
+For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
+
+## License
+
+GPL-3.0-or-later. See [LICENSE](LICENSE).
---
+++ src/client/java/io/github/keeningdawn/hungerinpeace/client/mixin/ExampleClientMixin.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2026 KeeningDawn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.keeningdawn.hungerinpeace.client.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class ExampleClientMixin {
+  @Inject(at = @At("HEAD"), method = "run")
+  private void init(CallbackInfo info) {
+    // This code is injected into the start of Minecraft.run()V
+  }
+}
